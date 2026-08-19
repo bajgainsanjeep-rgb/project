7c77d792f1eb847e716d895e07a5c4506af4b637
Sanjeep Bajgain <bajgainsanjeep@gmail.com>
2026-08-19 09:51:06 +0545

    hello

---
+++ contact.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'header.php';
+?>
+
+
+<h1>contact</h1>
+
+
+<?php
+require_once 'footer.php';
+?>
---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer>
+<p>
+    &copy; <?php echo date('Y');?>
+    CSIT. All rights reserved.
+</p>
+
+</footer>
+    
+</body>
+</html>
