60d1c3b36ea3ec3cd4037e05f56b50b15f59d066
Sanjeep Bajgain <bajgainsanjeep@gmail.com>
2026-08-21 09:38:13 +0545

    login

--- login.php
+++ login.php
@@ -1,9 +1,32 @@
 <?php
 require_once 'header.php';
+require_once 'connection.php';
+
+if(!empty($_POST)){
+    $email = $_POST['email'];
+    $password = md5($_POST['password']);
+
+    $query = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+    $result = mysqli_query($conn, $query);
+if(mysqli_num_rows($result) > 0){
+    echo "login successful";
+} else{
+    echo "invalid email or password";
+}
+}
+   
 ?>
 
 
 <h1>login</h1>
+
+<form action="" method="post">
+Email: <input type="email" name="email" required><br> <br>
+
+Password: <input type="password" name="password" required><br> <br>
+<button>login</button>
+
+</form>
 
 
 <?php
