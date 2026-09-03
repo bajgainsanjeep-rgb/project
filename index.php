7319c5a9895852b83f6afa2e2596a0e39f2637ee
Sanjeep Bajgain <bajgainsanjeep@gmail.com>
2026-09-03 11:20:22 +0545

    product details

--- index.php
+++ index.php
@@ -1,9 +1,31 @@
 <?php
 require_once 'header.php';
+require_once 'connection.php';
+
+$sql="SELECT * FROM products";
+$result=mysqli_query($conn,$sql);
 ?>
 
 
-<h1>welcome to our website!</h1>
+<h1>product list </h1>
+
+<ul>
+    <?php while($row = mysqli_fetch_assoc($result)): ?>
+        <li>
+            <img src="uploads/<?php echo $row['image']?>" width="200" height="200" alt="">
+            <h2><?php echo $row['title']; ?></h2>
+            <p><?php echo $row['description']; ?></p>
+            <p>Price: $<?php echo $row['price']; ?></p>
+        
+
+        <p>
+                <a href="product_detail.php?id=<?php echo $row['pid']; ?>">
+                    Product Details
+                </a> 
+            </p>
+        </li>
+    <?php endwhile; ?>
+</ul>
 
 
 <?php
